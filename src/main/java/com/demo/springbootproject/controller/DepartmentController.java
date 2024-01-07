c2c27ac9d4530c51c4556778dfd7bf8789d58cc7
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-01-07 12:42:41 +0530

    Complete getAllDepartments

--- src/main/java/com/demo/springbootproject/controller/DepartmentController.java
+++ src/main/java/com/demo/springbootproject/controller/DepartmentController.java
@@ -3,9 +3,12 @@ package com.demo.springbootproject.controller;
 import com.demo.springbootproject.entity.Department;
 import com.demo.springbootproject.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -13,8 +16,13 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
-    @PostMapping("/department")
+    @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department) {
         return departmentService.addDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
     }
 }
--- src/main/java/com/demo/springbootproject/service/DepartmentService.java
+++ src/main/java/com/demo/springbootproject/service/DepartmentService.java
@@ -5,14 +5,22 @@ import com.demo.springbootproject.repository.DepartmentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentService implements DepartmentServiceInterface{
 
     @Autowired
     private DepartmentRepo departmentRepo;
 
+
     @Override
     public Department addDepartment(Department department) {
         return departmentRepo.save(department);
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        return departmentRepo.findAll();
     }
 }
