9a15a2404165159ccd2cdf0a1b58fb93e28e2b53
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-01-08 21:02:22 +0530

    Add lombork
    Removed all getters & setters (boilerplate codes)

--- src/main/java/com/demo/springbootproject/entity/Department.java
+++ src/main/java/com/demo/springbootproject/entity/Department.java
@@ -5,8 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -16,56 +24,4 @@ public class Department {
     private String name;
     private String address;
     private String code;
-
-    public Department() {
-    }
-
-    public Department(Long id, String name, String address, String code) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.code = code;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", code='" + code + '\'' +
-                '}';
-    }
 }
